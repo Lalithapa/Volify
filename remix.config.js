7c9c444ec5918ce14e3d06af185357d450d843ff
Hujjat <hujjatnazari@gmail.com>
2024-08-26 18:04:12 +1000

    remix configs updated & polaris icon added to package.json

--- remix.config.js
+++ remix.config.js
@@ -9,7 +9,6 @@ if (
   process.env.SHOPIFY_APP_URL = process.env.HOST;
   delete process.env.HOST;
 }
-
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
   ignoredRouteFiles: ["**/.*"],
@@ -17,4 +16,26 @@ module.exports = {
   serverModuleFormat: "cjs",
   dev: { port: process.env.HMR_SERVER_PORT || 8002 },
   future: {},
+  serverDependenciesToBundle: [
+    "@shopify/polaris-viz",
+    "@juggle/resize-observer",
+    "@react-spring/animated",
+    "@react-spring/core",
+    "@react-spring/shared",
+    "@react-spring/types",
+    "@react-spring/web",
+    "@shopify/polaris-viz",
+    "@shopify/polaris-viz-core",
+    "d3-array",
+    "d3-color",
+    "d3-format",
+    "d3-interpolate",
+    "d3-path",
+    "d3-scale",
+    "d3-shape",
+    "d3-time",
+    "navigator",
+    "d3-time-format",
+    "internmap",
+    "use-debounce"],
 };
