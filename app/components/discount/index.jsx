3af3efab26e3e611919d16064b391eb61b735a49
Lalit Thapa <lalitthapa2108@gmail.com>
2025-12-16 17:42:35 +0530

    Create and Update Discounts

--- app/components/discount/index.jsx
+++ app/components/discount/index.jsx
@@ -14,7 +14,7 @@ export const DiscountPage = () => {
                 primaryAction={<Button variant="primary" url='/app/discounts/new'>New Discount</Button>}>
       <PageTitleBar title="Discount page" />
       <Card >
-        <EmptyState
+        {/* <EmptyState
         heading="Manage your discount"
         action={{content: 'Add Discount', url: '/app/discounts/new'}}
         secondaryAction={{
@@ -27,7 +27,40 @@ export const DiscountPage = () => {
             Create and discount your contents here. You can add, edit, and delete
             Discount.
         </p>
-        </EmptyState>
+        </EmptyState> */}
+    <s-section accessibilityLabel="Empty state section">
+        <s-grid gap="base" justifyItems="center" paddingBlock="large-400">
+          <s-box maxInlineSize="200px" maxBlockSize="200px">
+            {/* aspectRatio should match the actual image dimensions (width/height) */}
+            <s-image
+              aspectRatio="1/0.5"
+              src="https://cdn.shopify.com/static/images/polaris/patterns/callout.png"
+              alt="A stylized graphic of four characters, each holding a puzzle piece"
+            />
+          </s-box>
+          <s-grid justifyItems="center" maxInlineSize="450px" gap="base">
+            <s-stack alignItems="center">
+              <s-heading>Start creating discount</s-heading>
+              <s-paragraph>
+                Create and manage your collection of discount for players to enjoy.
+              </s-paragraph>
+            </s-stack>
+            <s-button-group>
+              <s-button
+                slot="secondary-actions"
+                aria-label="Learn more about creating discount"
+              >
+                {" "}
+                Learn more{" "}
+              </s-button>
+              <s-button slot="primary-action" aria-label="Add a new puzzle" href="/app/discounts/new">
+                {" "}
+                New Discount{" "}
+              </s-button>
+            </s-button-group>
+          </s-grid>
+        </s-grid>
+      </s-section>
         </Card>
     </PageLayout>
   );
--- app/routes/app.discounts.new.jsx
+++ app/routes/app.discounts.new.jsx
@@ -1,34 +1,130 @@
-import { Button, EmptyState, Card } from "@shopify/polaris";
+import { Button, EmptyState, Card, Text, Layout, BlockStack, TextField, Select, FormLayout } from "@shopify/polaris";
 import PageLayout from "../components/shared/pageLayout";
+import { useState } from "react";
 
 export default function Discounts() {
   // const { volumeDiscounts } = useLoaderData();
+  const [title, setTitle] = useState("");
+  const [discountValues, setDiscountValues] = useState([
+    {
+      title: "Buy one",
+      quantity: 1,
+      discount: 0,
+      discount_type: "percentage",
+      discount_message: "",
+      subtitle: "Standard price",
+      label: "",
+      badge: "",
+      selected: false,
+      label_bg: "#f7f7f7",
+      label_color: "#000000",
+      badge_bg: "#f55276",
+      badge_color: "#ffffff",
+    },
+    {
+      title: "Buy two get discount",
+      quantity: 2,
+      discount: 10,
+      discount_type: "percentage",
+      discount_message: "You save 10%",
+      subtitle: "You save 10%",
+      label: "Most popular",
+      badge: "Recommended",
+      selected: true,
+      label_bg: "#48cae4",
+      label_color: "#000000",
+      badge_bg: "#0096c7",
+      badge_color: "#ffffff",
+    },
+    {
+      title: "Buy three get discount",
+      quantity: 3,
+      discount: 20,
+      discount_type: "percentage",
+      discount_message: "You save 20%",
+      subtitle: "You save 20%",
+      label: "Best value",
+      badge: "Special offer",
+      selected: false,
+      label_bg: "#fbc4ab",
+      label_color: "#000000",
+      badge_bg: "#f08080",
+      badge_color: "#ffffff",
+    },
+  ]);
 
-  const headings = [
-    { title: 'Title' },
-    { title: 'isActive' },
-    { title: 'Created At' },
-    { title: 'Actions' },
-  ];
 
 
   return (
-    <PageLayout showBackButton title="Discounts" primaryAction={<Button variant="primary" url='/app/discounts/new'>New discount</Button>}>
-      <Card>
-          <EmptyState
-            heading="Manage your discounts"
-            action={{content: 'Add discount', url: '/app/discounts/new'}}
-            secondaryAction={{
-              content: 'Learn more',
-              url: 'https://help.shopify.com',
-            }}
-            image="https://cdn.shopify.com/b/shopify-guidance-dashboard-public/m66z0a57ues1gygrane8proz6gqn.svgz"
-          >
-            <p>
-              Create discounts for your products and collections.
-            </p>
-          </EmptyState>
-        </Card>
+    <PageLayout showBackButton title="Discounts" >
+      <s-grid
+        gridTemplateColumns="repeat(3, 1fr)"
+        gap="large-100"
+        justifyContent="center"
+      >
+        <s-grid-item gridColumn="span 2" >
+        <BlockStack gap="400">
+          <s-section heading="Basic Settings">
+            <BlockStack gap="400">
+              <TextField
+                label="Discount Title"
+                defaultValue="Buy one" value={title} onChange={(value) => setTitle(value)}
+              />
+              </BlockStack>
+          </s-section>
+            { discountValues.map((item, index) => (
+                <Card key={index} sectioned>
+                  <BlockStack gap="200">
+                    <Text variant="headingMd" as="h2" fontWeight="semibold">
+                      Discount {index + 1}
+                    </Text>
+                    <BlockStack gap="200">
+                       <FormLayout.Group condensed>
+                      <TextField
+                        label="Title"
+                        defaultValue="Buy one" value={item.title} onChange={(value) => {
+                          const newDiscountValues = [...discountValues];
+                          newDiscountValues[index].title = value;
+                          setDiscountValues(newDiscountValues);
+                        }}
+                      />
+                      <TextField
+                        label="Quantity"
+                        type="number"
+                         defaultValue={item.quantity.toString()} value={item.quantity.toString()} onChange={(value) => {
+                          const newDiscountValues = [...discountValues];
+                          newDiscountValues[index].quantity = parseInt(value);
+                          setDiscountValues(newDiscountValues);
+                        }}
+                      />
+                      </FormLayout.Group>
+                      <Select
+                        label="Discount type"
+                        options={[
+                          { label: "Percentage", value: "percentage" },
+                          { label: "Fixed amount", value: "fixed" },
+                        ]}
+                      />
+                      <TextField
+                        label="Discount value"
+                        type="number"
+                        suffix="%"
+                      />
+                    </BlockStack>
+                  </BlockStack>
+                </Card>
+            ))}
+          </BlockStack>
+        </s-grid-item>
+        <s-grid-item gridColumn="span 1">
+          <Card>
+              <Text variant="headingMd" as="h2" fontWeight="semibold" blockAlign="center">
+                Discount Name : {title}
+              </Text>
+          </Card>
+        </s-grid-item>
+      </s-grid>
+
     </PageLayout>
   );
 }
